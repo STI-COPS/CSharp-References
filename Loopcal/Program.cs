e11b728d5a18218c3a334165492713f64866bec6
Club Of Programmers <114017269+STI-COPS@users.noreply.github.com>
2022-10-10 16:35:56 +0800

    Loop Calculator made in C#

---
+++ Loopcal/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Loopcal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Addition \n2. Subtaction \n3. Multiplication \n4. Division \n5. End");
+                Console.Write("Choose:");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 1)
+                {
+                    Console.Write("First Number:");
+                    int inp1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second Number:");
+                    int inp2 = Convert.ToInt32(Console.ReadLine());
+                    int sum = inp1 + inp2;
+                    Console.WriteLine("The answer is: " + sum);
+                }
+                else if (input == 2)
+                {
+                    Console.Write("First Number:");
+                    int inp1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second Number:");
+                    int inp2 = Convert.ToInt32(Console.ReadLine());
+                    int sum = inp1 - inp2;
+                    Console.WriteLine("The answer is: " + sum);
+                }
+                else if (input == 3)
+                {
+                    Console.Write("First Number:");
+                    int inp1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second Number:");
+                    int inp2 = Convert.ToInt32(Console.ReadLine());
+                    int sum = inp1 * inp2;
+                    Console.WriteLine("The answer is: " + sum);
+                }
+                else if (input == 4)
+                {
+                    Console.Write("First Number:");
+                    int inp1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second Number:");
+                    int inp2 = Convert.ToInt32(Console.ReadLine());
+                    int sum = inp1 / inp2;
+                    Console.WriteLine("The answer is: " + sum);
+                }
+                else
+                {
+                    Console.Write("First Number:");
+                    int inp1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Second Number:");
+                    int inp2 = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (input != 5);
+
+            }
+            }
+            
+    }
+    
